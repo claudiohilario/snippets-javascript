f1727e201e74784c20c705195c3e63e88b0a38c8
Cláudio Hilário <claudio.hilario@ontech.pt>
2020-05-24 14:49:35 +0100

    Update sortArrayOfObjectsByNumber.js

--- sortArrayOfObjectsByNumber.js
+++ sortArrayOfObjectsByNumber.js
@@ -1,22 +1,22 @@
 const sortArrayOfObjectsByNumber = (arrayOfObjects = [], key = 'order') => {
 	const mapper = {
   	ASC: (a, b) => (a[key] - b[key]),
-    DESC: (a, b) => (b[key] - a[key]),
+        DESC: (a, b) => (b[key] - a[key]),
   }
   
   return {
   	asc: () => arrayOfObjects.sort(mapper['ASC']),
-    desc: () => arrayOfObjects.sort(mapper['DESC']),
+        desc: () => arrayOfObjects.sort(mapper['DESC']),
   }
 }
 
 /** Example */
 const arrTest = [
 	{name: 'One', order: 10},
-  {name: 'Two', order: 1},
-  {name: 'Three', order: 15},
-  {name: 'Four', order: 11},
-  {name: 'Five', order: 5},
+  	{name: 'Two', order: 1},
+  	{name: 'Three', order: 15},
+  	{name: 'Four', order: 11},
+  	{name: 'Five', order: 5},
 ];
 
 const sortArrayOfObjectsByNumberTest = sortArrayOfObjectsByNumber(arrTest);
